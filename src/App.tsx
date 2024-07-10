222dc5ca77a9dd51121bbfa5b8617d777687ca2c
xieyijia <xlin84395@gmail.com>
2024-07-10 10:51:16 +0800

    feat:发布测试

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + 发布测试</h1>
+      <h1>Vite + React + 发布测试 + LIN</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
